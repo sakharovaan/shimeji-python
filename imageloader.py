f9fed050d2ce393785d204ed0066e2dc4540e68a
Alexander <mail@asakharov.ru>
2021-11-19 00:15:46 +0300

    [stable] some logging

--- imageloader.py
+++ imageloader.py
@@ -26,21 +26,24 @@ class ImageLoader:
 
     def _loadimage(self, path, confsource="ghost"):
         if path in self._imagecache:
+            logging.debug("got " + path + " from cache")
             return self._imagecache[path]
         else:
             with self.RBGAImage(path) as im:
                 wpercent = (self._config[confsource]['width'] / float(im.size[0]))
                 hsize = int((float(im.size[1]) * float(wpercent)))
                 self._imagecache[path] = im.resize((self._config[confsource]['width'], hsize), Image.BICUBIC)
+            logging.debug("rendered " + path )
 
             return self._imagecache[path]
 
     def _generate_image(self, **kwargs):
-        op = self._loadimage(self._config['background']['file'])
+        op = self._loadimage(self._config['background']['file']).copy()
 
         for f in list(self._config['elements']['eyes'].get(kwargs['eyes'])[kwargs['state']]['file'] +
                       self._config['elements']['eyebrows'].get(kwargs['eyebrows'])['file'] +
                       self._config['elements']['mouth'].get(kwargs['mouth'])['file']):
+            logging.debug("adding layer " + f)
             op = Image.alpha_composite(op, self._loadimage(f))
 
         return ImageTk.PhotoImage(op)
@@ -49,7 +52,10 @@ class ImageLoader:
         k = "_".join(["{}={}".format(*x) for x in kwargs.items()])
         logging.debug("  ")
         if k not in self._imagebase:
+            logging.debug("generating " + k)
             self._imagebase[k] = self._generate_image(**kwargs)
+        else:
+            logging.debug("got " + k + " from cache")
 
         return self._imagebase[k]
 
